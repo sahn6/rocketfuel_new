9f3b84648ca612f68f0a94441b9b7793b3c0dc7f
sahn6 <sahn6@ncsu.edu>
2018-04-07 00:31:57 -0400

    initial

--- GeoIP-1.3.2/tests/test_netspeedcell.py
+++
@@ -1,8 +0,0 @@
-import GeoIP
-from nose.tools import assert_equal
-
-
-def test_netspeed_cell():
-    gi = GeoIP.open("tests/data/GeoIPNetSpeedCell.dat", GeoIP.GEOIP_STANDARD)
-
-    assert_equal(gi.org_by_addr("2.125.160.1"), 'Dialup')
